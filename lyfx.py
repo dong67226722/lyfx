605a7d74e5710e5215309d988ffbe348e31680ca
dong67226722 <huafa.dong@groupfu.com>
2020-06-14 00:00:18 +0800

    calc len

--- lyfx.py
+++ lyfx.py
@@ -1,7 +1,15 @@
 data = []
+count = 0
 with open("lyfx.txt", "r") as f:
 	for line in f:
 		data.append(line)
-print(len(data))
+		count += 1
+		if count % 2000 == 0:
+			print(len(data))
+print("档案读取完成了，总共有", len(data), "笔资料")
 
-print(data[3])
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+
+print("留言的平均长度是：", sum_len / len(data))
