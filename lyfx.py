79cbd410fb17bc944bb6996e92843a12be873abe
dong67226722 <huafa.dong@groupfu.com>
2020-06-17 22:01:14 +0800

    add word count

--- lyfx.py
+++ lyfx.py
@@ -8,6 +8,8 @@ with open("lyfx.txt", "r") as f:
 			print(len(data))
 print("档案读取完成了，总共有", len(data), "笔资料")
 
+print(data[0])
+
 sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
@@ -27,3 +29,34 @@ for d in data:
 	if "love" in d:
 		love.append(d)
 print("一共有", len(love), "笔资料提到love")
+
+wc = {}
+for d in data:
+	words = d.split(' ')
+	for word in words:
+		if word in wc:
+			wc[word] += 1
+		else:
+			wc[word] = 1
+
+for word in wc:
+	if wc[word] > 100000:
+		print(word, wc[word])
+
+print(len(wc))
+
+
+while True:
+	word = input('请输入要查询的字：')
+	if word == 'q':
+		break
+	if word in wc:
+		print(word ,'出现的次数为：', wc[word])
+	else:
+		print('你查的字没有出现过！')
+
+print('感谢使用查询功能！')
+
+
+
+
